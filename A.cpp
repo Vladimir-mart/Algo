87c0cb2a3eb1d49665108fbbf5c6f99990fed186
V-Martyanov <martyanov@phystech.edu>
2023-04-07 19:28:43 +0300

    sqriptBash

--- A.cpp
+++ A.cpp
@@ -1,4 +1,3 @@
-
 #include <cstdio>
 #include <iostream>
 #include <queue>
@@ -15,15 +14,14 @@ public:
   virtual vector<T1> RetS(T1 a) = 0;
   virtual void SetV() = 0;
   virtual void SetWay(T1 a, T2 b) = 0;
-
   virtual int Stat() = 0;
   virtual int Fin() = 0;
 };
 
 template <typename T1, typename T2, typename K>
-class CraphOnMatrix : public Graph<T1, T2, K> {
+class GraphOnMatrix : public Graph<T1, T2, K> {
 public:
-  CraphOnMatrix(int ko, int reb);
+  GraphOnMatrix(int ko, int reb);
   int RetV();
   vector<T1> RetS(T1 a);
   void SetV();
@@ -32,104 +30,132 @@ public:
   int Fin() { return finish; }
 
   class OnIter {
-    //std::iterator<std::input_iterator_tag>
   public:
     int* sp = nullptr;
     int* fp = nullptr;
-    OnIter(int* a, int* b) {
-      sp = a;
-      fp = b;
-    }
+    OnIter(int* a, int* b);
     class Iterator {
     public:
+      Iterator() : p(nullptr) {}
+      Iterator(int* pp) { p = pp; }
+      Iterator(const Iterator& st) : p(st.p), pos(st.pos), v(st.pos) {}
+      Iterator& operator++();
+      int& operator*();
+      Iterator& operator--();
+      Iterator& operator=(const Iterator& other);
+      friend bool operator==(Iterator first, Iterator second);
+      friend bool operator!=(Iterator first, Iterator second);
+    private:
       int* p = nullptr;
       int v = 0;
       int pos = 0;
-      Iterator() : p(nullptr) {}
-      Iterator(int* pp) {
-        p = pp;
-      }
-      Iterator(const Iterator& st) {
-        p = st.p;
-        pos = st.pos;
-        v = st.v;
-      }
-      Iterator& operator++() {
-        for (int i = 0; *p != -1; ++pos, ++i) {
-          if (*p == 1 and i > 0) {
-            break;
-          }
-          else {
-            p = (p + 1);
-          }
-        }
-        return *this;
-      }
-      int& operator*() { return pos; }
-      Iterator& operator--() {
-        for (int i = 0; pos >= 0; --pos, ++i) {
-          if (*p == 1 and i > 0) {
-            break;
-          }
-          else {
-            p = (p - 1);
-          }
-        }
-        return *this;
-      }
-      friend bool operator==(Iterator first, Iterator second) {
-        return first.p == second.p;
-      }
-      friend bool operator!=(Iterator first, Iterator second) {
-        return !(first == second);
-      }
-      Iterator& operator=(const Iterator& other) {
-        p = other.p;
-        pos = other.pos;
-        v = other.v;
-        return *this;
-      }
     };
-    Iterator begin() {
-      Iterator temp(sp);
-      if (*sp == 0) {
-        ++temp;
-      }
-      return temp;
-    }
-    Iterator end() {
-      return Iterator(fp);
-    }
+    Iterator begin();
+    Iterator end() { return Iterator(fp); }
   };
-  OnIter RangeB(T1 ver) {
-    return OnIter(&g[ver][0], &g[ver][g[ver].size() - 1]);
-  }
+  OnIter RangeB(T1 ver);
+
 private:
   int kolvo = 0;
   int reb = 0;
-  T1 start;
-  T2 finish;
-  int u;
-  int v;
+  T1 start = 0;
+  T2 finish = 0;
+  int t_from = 0;
+  int t_to = 0;
   vector<T1> dist, parent;
   vector<vector<T1>> g;
 };
 
+
+
 template <typename T1, typename T2, typename K>
-vector<T1> CraphOnMatrix<T1, T2, K>::RetS(T1 a) {
+bool operator!=(typename GraphOnMatrix<T1, T2, K>::OnIter::Iterator first, typename GraphOnMatrix<T1, T2, K>::OnIter::Iterator second) {
+  return !(first == second);
+}
+
+
+template <typename T1, typename T2, typename K>
+bool operator==(typename GraphOnMatrix<T1, T2, K>::OnIter::Iterator first, typename GraphOnMatrix<T1, T2, K>::OnIter::Iterator second) {
+  return first.p == second.p;
+}
+
+template <typename T1, typename T2, typename K>
+typename GraphOnMatrix<T1, T2, K>::OnIter GraphOnMatrix<T1, T2, K>::RangeB(T1 ver) {
+  return OnIter(&g[ver][0], &g[ver][g[ver].size() - 1]);
+}
+
+template <typename T1, typename T2, typename K>
+typename GraphOnMatrix<T1, T2, K>::OnIter::Iterator GraphOnMatrix<T1, T2, K>::OnIter::begin() {
+  Iterator temp(sp);
+  if (*sp == 0) {
+    ++temp;
+  }
+  return temp;
+}
+
+template <typename T1, typename T2, typename K>
+GraphOnMatrix<T1, T2, K>::OnIter::OnIter(int* a, int* b) {
+  sp = a;
+  fp = b;
+}
+
+template <typename T1, typename T2, typename K>
+typename GraphOnMatrix<T1, T2, K>::OnIter::Iterator&
+GraphOnMatrix<T1, T2, K>::OnIter::Iterator::operator=(const Iterator& other) {
+  p = other.p;
+  pos = other.pos;
+  v = other.v;
+  return *this;
+}
+
+template <typename T1, typename T2, typename K>
+typename GraphOnMatrix<T1, T2, K>::OnIter::Iterator&
+GraphOnMatrix<T1, T2, K>::OnIter::Iterator::operator--() {
+  for (int i = 0; pos >= 0; --pos, ++i) {
+    if (*p == 1 and i > 0) {
+      break;
+    }
+    else {
+      p = (p - 1);
+    }
+  }
+  return *this;
+}
+
+template <typename T1, typename T2, typename K>
+int& GraphOnMatrix<T1, T2, K>::OnIter::Iterator::operator*() {
+  return pos;
+}
+
+template <typename T1, typename T2, typename K>
+typename GraphOnMatrix<T1, T2, K>::OnIter::Iterator&
+GraphOnMatrix<T1, T2, K>::OnIter::Iterator::operator++() {
+  for (int i = 0; *p != -1; ++pos, ++i) {
+    if (*p == 1 and i > 0) {
+      break;
+    }
+    else {
+      p = (p + 1);
+    }
+  }
+  return *this;
+}
+
+template <typename T1, typename T2, typename K>
+vector<T1> GraphOnMatrix<T1, T2, K>::RetS(T1 a) {
   return g[a];
 }
 
 template <typename T1, typename T2, typename K>
-void CraphOnMatrix<T1, T2, K>::SetV() {
+void GraphOnMatrix<T1, T2, K>::SetV() {
   g.resize(kolvo + 1);
   for (int i = 0; i <= kolvo; ++i) {
     g[i].resize(kolvo + 1);
   }
   for (int j = 0; j < reb; ++j) {
-    cin >> u >> v;
-    g[u][v] = 1;
-    g[v][u] = 1;
+    cin >> t_from >> t_to;
+    g[t_from][t_to] = 1;
+    g[t_to][t_from] = 1;
   }
   for (int i = 0; i <= kolvo; ++i) {
     g[i].push_back(-1);
@@ -137,27 +163,26 @@ void CraphOnMatrix<T1, T2, K>::SetV() {
 }
 
 template <typename T1, typename T2, typename K>
-void CraphOnMatrix<T1, T2, K>::SetWay(T1 a, T2 b) {
+void GraphOnMatrix<T1, T2, K>::SetWay(T1 a, T2 b) {
   this->start = a;
   this->finish = b;
 }
 
 template <typename T1, typename T2, typename K>
-CraphOnMatrix<T1, T2, K>::CraphOnMatrix(int ko, int reb) {
+GraphOnMatrix<T1, T2, K>::GraphOnMatrix(int ko, int reb) {
   this->kolvo = ko;
   this->reb = reb;
 }
 
 template <typename T1, typename T2, typename K>
-int CraphOnMatrix<T1, T2, K>::RetV() {
+int GraphOnMatrix<T1, T2, K>::RetV() {
   return kolvo;
 }
 
 template <typename T1, typename T2, typename K>
-class CraphOnVector : public Graph<T1, T2, K> {
-  // я не добавлял map к различным типам
+class GraphOnVector : public Graph<T1, T2, K> {
 public:
-  CraphOnVector(int ko, int reb);
+  GraphOnVector(int ko, int reb);
   int RetV();
   vector<T1> RetS(T1 a);
   void SetV();
@@ -165,94 +190,107 @@ public:
   int Stat() { return start; }
   int Fin() { return finish; }
   class OnIter {
-    //std::iterator<std::input_iterator_tag>
   public:
     int* sp = nullptr;
     int* fp = nullptr;
-    OnIter(int* a, int* b) {
-      sp = a;
-      fp = b;
-    }
+    OnIter(int* a, int* b);
     class Iterator {
     public:
       int* p = nullptr;
       int v = 0;
       int pos = 0;
       Iterator() : p(nullptr) {}
-      Iterator(int* pp) {
-        p = pp;
-      }
-      Iterator(const Iterator& st) {
-        p = st.p;
-        pos = st.pos;
-        v = st.v;
-      }
-      Iterator& operator++() {
-        if (*p != -1) {
-          p = (p + 1);
-          pos++;
-        }
-        return *this;
-      }
+      Iterator(int* pp) { p = pp; }
+      Iterator(const Iterator& st);
+      Iterator& operator++();
       int& operator*() { return *p; }
-      Iterator& operator--() {
-        if (pos >= 0) {
-          p = (p - 1);
-          pos--;
-        }
-        return *this;
-      }
+      Iterator& operator--();
       friend bool operator==(Iterator first, Iterator second) {
         return first.p == second.p;
       }
       friend bool operator!=(Iterator first, Iterator second) {
         return !(first == second);
       }
-      Iterator& operator=(const Iterator& other) {
-        p = other.p;
-        pos = other.pos;
-        v = other.v;
-        return *this;
-      }
+      Iterator& operator=(const Iterator& other);
     };
-    Iterator begin() {
-      Iterator temp(sp);
-      if (*sp == 0) {
-        ++temp;
-      }
-      return temp;
-    }
-    Iterator end() {
-      return Iterator(fp);
-    }
+    Iterator begin();
+    Iterator end() { return Iterator(fp); }
   };
   OnIter RangeB(T1 ver) {
     return OnIter(&g[ver][0], &g[ver][g[ver].size() - 1]);
   }
+
 private:
   int kolvo = 0;
   int reb = 0;
-  T1 start;
-  T2 finish;
-  int u;
-  int v;
+  T1 start = 0;
+  T2 finish = 0;
+  int t_from = 0;
+  int t_to = 0;
   vector<T1> dist, parent;
   vector<vector<T1>> g;
-  // зачем я это делал,? - не знаю
 };
 
 template <typename T1, typename T2, typename K>
-vector<T1> CraphOnVector<T1, T2, K>::RetS(T1 a) {
+typename GraphOnVector<T1, T2, K>::OnIter::Iterator GraphOnVector<T1, T2, K>::OnIter::begin() {
+  Iterator temp(sp);
+  if (*sp == 0) {
+    ++temp;
+  }
+  return temp;
+}
+
+template <typename T1, typename T2, typename K>
+typename GraphOnVector<T1, T2, K>::OnIter::Iterator& GraphOnVector<T1, T2, K>::OnIter::Iterator::operator=(const typename GraphOnVector<T1, T2, K>::OnIter::Iterator& other) {
+  p = other.p;
+  pos = other.pos;
+  v = other.v;
+  return *this;
+}
+
+template <typename T1, typename T2, typename K>
+typename GraphOnVector<T1, T2, K>::OnIter::Iterator& GraphOnVector<T1, T2, K>::OnIter::Iterator::operator--() {
+  if (pos >= 0) {
+    p = (p - 1);
+    pos--;
+  }
+  return *this;
+}
+
+template <typename T1, typename T2, typename K>
+typename GraphOnVector<T1, T2, K>::OnIter::Iterator& GraphOnVector<T1, T2, K>::OnIter::Iterator::operator++() {
+  if (*p != -1) {
+    p = (p + 1);
+    pos++;
+  }
+  return *this;
+}
+
+template <typename T1, typename T2, typename K>
+GraphOnVector<T1, T2, K>::OnIter::Iterator::Iterator(const typename GraphOnVector<T1, T2, K>::OnIter::Iterator& st) {
+  p = st.p;
+  pos = st.pos;
+  v = st.v;
+}
+
+template <typename T1, typename T2, typename K>
+GraphOnVector<T1, T2, K>::OnIter::OnIter(int* a, int* b) {
+  sp = a;
+  fp = b;
+}
+
+template <typename T1, typename T2, typename K>
+vector<T1> GraphOnVector<T1, T2, K>::RetS(T1 a) {
   return g[a];
 }
 
 template <typename T1, typename T2, typename K>
-void CraphOnVector<T1, T2, K>::SetV() {
+void GraphOnVector<T1, T2, K>::SetV() {
   g.resize(kolvo + 1);
   for (int j = 0; j < reb; ++j) {
-    cin >> u >> v;
-    g[u].push_back(v);
-    g[v].push_back(u);
+    cin >> t_from >> t_to;
+    g[t_from].push_back(t_to);
+    g[t_to].push_back(t_from);
   }
   for (int i = 0; i <= kolvo; ++i) {
     g[i].push_back(-1);
@@ -260,19 +298,19 @@ void CraphOnVector<T1, T2, K>::SetV() {
 }
 
 template <typename T1, typename T2, typename K>
-void CraphOnVector<T1, T2, K>::SetWay(T1 a, T2 b) {
+void GraphOnVector<T1, T2, K>::SetWay(T1 a, T2 b) {
   this->start = a;
   this->finish = b;
 }
 
 template <typename T1, typename T2, typename K>
-CraphOnVector<T1, T2, K>::CraphOnVector(int ko, int reb) {
+GraphOnVector<T1, T2, K>::GraphOnVector(int ko, int reb) {
   this->kolvo = ko;
   this->reb = reb;
 }
 
 template <typename T1, typename T2, typename K>
-int CraphOnVector<T1, T2, K>::RetV() {
+int GraphOnVector<T1, T2, K>::RetV() {
   return kolvo;
 }
 
@@ -288,7 +326,6 @@ class HolVisitor : public Visitor {
 private:
   vector<int> dist;
   vector<int> perent;
-
 public:
   HolVisitor(int kolvo);
   void Pvisit(int v, int to) override;
@@ -331,9 +368,9 @@ vector<int> Rez(Visitor* vis, int finish) {
   }
   return path;
 }
-// вот БФС со всеми введениями, почему CraphOnVector, ну у меня там класс
-// итератора, begin и end до них ни как не достучатся от Craph
-vector<int> Bfs(Visitor* vis, CraphOnVector<int, int, int>& st) {
+// тут БФС со всем введениями, а так же второй пункт, я сделал без класса, мне так убдобней
+// принимает граф и визитор, а возвращать вектор вершин(шаблонных), что лежат на пути.
+vector<int> Bfs(Visitor* vis, GraphOnVector<int, int, int>& st) {
   // из дефолта, сказали как нибудь
   vis->Dvisit(0, st.Stat());
   queue<int> q;
@@ -350,14 +387,15 @@ vector<int> Bfs(Visitor* vis, CraphOnVector<int, int, int>& st) {
       }
     }
   }
-/*
-*   Хитро?
-    for (auto iter : st.RetS(v)) {
-      iter = 10; // но это будет не работать
-    }
-*/
+  /*
+  *   Хитро?
+      for (auto iter : st.RetS(v)) {
+        iter = 10; // но это будет не работать
+      }
+  */
   return Rez(vis, st.Fin());
 }
+
 
 int main() {
   int n;
@@ -366,45 +404,44 @@ int main() {
   int b;
   cin >> n >> m;
   cin >> a >> b;
+  GraphOnVector<int, int, int> A(n, m);
   // Graph<int, int, int> *B = nullptr;
-  // CraphOnVector<int, int, int> A(n, m);
   // B = &A;
   // B->SetWay(a, b);
   // B->SetV();
   // B->BFS(a);
   // B->Rez();
-  // A.SetWay(a, b);
-  // 
-  // Visitor *v;
-  // HolVisitor hv(n);
-  // v = &hv;
-  // vector<int> path = Bfs(v, A);
-  // for (int i = int(path.size() - 1); i >= 0; i--) {
-  //   cout << path[i] << " ";
-  // }
-// Матрица работате
-/*
-  CraphOnMatrix<int, int, int> A(n, m);
   A.SetWay(a, b);
   A.SetV();
-  for (auto itt : A.RangeB(1)) {
-    cout << itt;
+  Visitor* v;
+  HolVisitor hv(n);
+  v = &hv;
+  vector<int> path = Bfs(v, A);
+  for (int i = int(path.size() - 1); i >= 0; i--) {
+    cout << path[i] << " ";
   }
-*/
-// Список работате
-/*
-  CraphOnVector<int, int, int> A(n, m);
-  A.SetWay(a, b);
-  A.SetV();
-  for (auto itt : A.RangeB(1)) {
-    cout << itt;
-  }
-*/
+  // Матрица работате
+  /*
+    GraphOnMatrix<int, int, int> A(n, m);
+    A.SetWay(a, b);
+    A.SetV();
+    for (auto itt : A.RangeB(1)) {
+      cout << itt;
+    }
+  */
+  // Список работате
+  /*
+    GraphOnVector<int, int, int> A(n, m);
+    A.SetWay(a, b);
+    A.SetV();
+    for (auto itt : A.RangeB(1)) {
+      cout << itt;
+    }
+  */
   return 0;
 }
 
 /*
 * P.S
-  знаю, много говна и не правильно, но я не понял ревью и не кодил год
+  знаю, много говна, прошу сильно не бить ♥
 */
-
